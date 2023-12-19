3af32cf546610030e1c2ca17e771d436c43f2d58
Zekiloni <zekirija17@gmail.com>
2023-12-19 18:29:12 +0100

    sex db type to enum with values

--- database/migrations/2023_12_18_220532_create_students_table.php
+++ database/migrations/2023_12_18_220532_create_students_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->date('date_of_birth');
-            $table->string('sex');
+            $table->enum('sex', ['male', 'female']);
             $table->string('email');
             $table->string('phone_number');
             $table->text('address');
